737fa8f7923e9d48ea650f702fd9f7e86c4be758
Ravikant Tiwary <tiwariravikant04@gmail.com>
2024-10-26 23:14:32 +0530

    added preloader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
@@ -6,10 +7,36 @@ import { TestimonialsSection } from "@/sections/Testimonials";
 import { AboutSection } from "@/sections/About";
 import { ContactSection } from "@/sections/Contact";
 import { Footer } from "@/sections/Footer";
+import Preloader from '../components/Preloader';
+import { AnimatePresence, useScroll } from 'framer-motion';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start start', 'end end']
+  });
+
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import('locomotive-scroll')).default;
+      const locomotiveScroll = new LocomotiveScroll();
+
+      setTimeout(() => {
+        setIsLoading(false);
+        document.body.style.cursor = 'default';
+        window.scrollTo(0, 0);
+      }, 2000);
+    })();
+  }, []);
+
   return (
     <>
+      <AnimatePresence mode='wait'>
+        {isLoading && <Preloader />}
+      </AnimatePresence>
       <Header />
       <HeroSection />
       <ProjectsSection />
