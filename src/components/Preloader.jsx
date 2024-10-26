b9eeb529a68d27a4f5e53b1205c4d1492dd67d8b
Ravikant Tiwary <tiwariravikant04@gmail.com>
2024-10-27 02:39:26 +0530

    Refactor Preloader transition duration in Index component and add express dependency

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -48,7 +48,7 @@ export default function Index() {
         },
         exit: {
             top: "-100vh",
-            transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.2 }
+            transition: { duration: 1, ease: [0.76, 0, 0.24, 1], delay: 0.2 }
         }
     }
 
